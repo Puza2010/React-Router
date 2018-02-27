004b6f98b1ddf4a61b8a3f39d1c8beda3801ce41
Przemek <puza147@gmail.com>
2018-02-27 13:13:48 +0100

    update

--- src/presentational/flag.component.js
+++ src/presentational/flag.component.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import styles from './flag.css';
 
 const CountryFlag = (props) => (
-    <div className="country-logo-wrapper">
-        <img className="country-logo" src={props.country.imageUrl} alt="country photo" />
+    <div className={styles.flag}>
+        <div className="country-logo-wrapper">
+            <img className="country-logo" src={props.country.imageUrl} alt="country photo" />
+        </div>
     </div>
 );
 
